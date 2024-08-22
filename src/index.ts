53f90e2d1ee1e019e35e5344e0eccd22a6d9878c
Keith Daulton <kdaulton@d13design.com>
2024-08-21 21:56:56 -0400

    Resolves #12 add icon component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Badge } from './badge';
 export { Button } from './button';
 export { Divider } from './divider';
+export { Icon } from './icon';
 export { Label } from './label';
 export { Link } from './link';
